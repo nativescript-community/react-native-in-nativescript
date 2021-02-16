6d3239bd69947b71d8a6cd01d7ac19b1e8001c7e
Jamie Birch work laptop <jbizzle5@gmail.com>
2021-02-16 09:04:40 +0000

    Show how we'd apply the delegate

--- apps/demo-react/src/app.ts
+++ apps/demo-react/src/app.ts
@@ -1,16 +1,11 @@
 import * as React from 'react';
-
 /* Controls react-nativescript log verbosity. true: all logs; false: only error logs. */
 Object.defineProperty(global, '__DEV__', { value: false });
-
-/*
-In NativeScript, the app.ts file is the entry point to your application.
-You can use this file to perform app-level initialization, but the primary
-purpose of the file is to pass control to the app’s first module.
-*/
-
 import * as ReactNativeScript from 'react-nativescript';
 import { mainStackNavigator as AppContainer } from './components/Navigator';
+// import { MyDelegate } from './AppDelegate';
+// import { Application } from '@nativescript/core';
+// Application.ios.delegate = MyDelegate;
 
 ReactNativeScript.start(React.createElement(AppContainer, {}, null));
 
